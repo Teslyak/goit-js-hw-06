0cbd99b27b42dc1df5076c278bf708a2feb97430
Teslyak <124871929+Teslyak@users.noreply.github.com>
2023-06-13 16:09:43 +0300

    hw6-task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const arrIngr = [];
+const makeElList = options => {
+  const el = document.createElement('li');
+  el.textContent = options;
+  el.classList.add('item');
+  arrIngr.push(el);
+  
+};
+const ingrEl = ingredients.forEach(elm => {
+  makeElList(elm); 
+});
+const ingrList = document.querySelector('#ingredients');
+ingrList.append(...arrIngr);
+
+

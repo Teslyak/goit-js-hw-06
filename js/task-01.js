938f6661144ebdcae3b0e0e46dea956774f45c42
Teslyak <124871929+Teslyak@users.noreply.github.com>
2023-06-13 15:03:24 +0300

    tasl-1 - upd

--- js/task-01.js
+++ js/task-01.js
@@ -3,7 +3,7 @@ const itemList = document.querySelectorAll('.item');
 console.log(`Number of categories: ${itemList.length}`);
 const nameCategoriesItem = itemList.forEach(el => {
     const nameCategories = el.firstElementChild.textContent;
-    const quantityElementsItem =el.lastElementChild.children.length
+    const quantityElementsItem = el.lastElementChild.children.length;
     console.log(`Category: ${nameCategories}\nElements: ${quantityElementsItem}`);
 })
 

0ca59f7e113080204ca03ab8b6895a981ca68d26
Teslyak <124871929+Teslyak@users.noreply.github.com>
2023-06-15 10:12:54 +0300

    hw6- task-4

---
+++ js/task-04.js
@@ -0,0 +1,19 @@
+let counterValue = 0;
+const refs = {
+    btnDecrm: document.querySelector('[data-action="decrement"]'),
+    btnIncrm: document.querySelector('[data-action="increment"]'),
+    spanCounterValue: document.querySelector('#value')
+};
+
+refs.btnDecrm.addEventListener('click',onButtonDecrement);
+refs.btnIncrm.addEventListener('click',onButtonIncrement);
+
+function onButtonDecrement() {
+    counterValue -= 1;
+    refs.spanCounterValue.textContent = counterValue;
+};
+function onButtonIncrement() {
+    counterValue += 1;
+    refs.spanCounterValue.textContent = counterValue;
+    
+};

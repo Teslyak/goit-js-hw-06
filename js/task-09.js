2656abc656f617e17c7832c861e593c790672fa1
Teslyak <124871929+Teslyak@users.noreply.github.com>
2023-06-16 15:37:39 +0300

    hw6-task9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  button: document.querySelector('.change-color'),
+  body: document.querySelector('body'),
+  span: document.querySelector('.color')
+}
+refs.button.addEventListener('click', onclickBtnChangeColor);
+
+function onclickBtnChangeColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.span.textContent = refs.body.style.backgroundColor;
+
+}

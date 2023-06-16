ddba5a5ff3bd22b73067f637927785f32d685489
Teslyak <124871929+Teslyak@users.noreply.github.com>
2023-06-16 15:22:02 +0300

    hw6- task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const refs = {
+form: document.querySelector('.login-form')
+}
 
+
+refs.form.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+    event.preventDefault();
+    const { email, password } = event.currentTarget.elements;
+    const emailValue = email.value.toLowerCase();
+    const passwordValue = password.value;
+    if (!emailValue || !passwordValue) {
+        alert('Будь ласка заповніть всі поля');
+    };
+    const makeObjectFormData = {
+        email: emailValue,
+        password: passwordValue
+    };
+    event.currentTarget.reset();
+    
+};

2b513923630ab4cf75b43712872039f3499f27f8
Teslyak <124871929+Teslyak@users.noreply.github.com>
2023-06-16 16:23:42 +0300

    hw6 start-task10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,20 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  inputAmount: document.querySelector('[type=number]'),
+  buttonCreate: document.querySelector('[data-create]'),
+  buttonDestroy: document.querySelector('[data-destroy]'),
+  divBoxes: document.querySelector('#boxes')
+
+}
+
+refs.inputAmount.addEventListener('submit', onInputAmount);
+
+function onInputAmount() {
+  
+};
+
+
+

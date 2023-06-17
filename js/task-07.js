1d4eafce4b7df666c0aab953229efc9e2ce2af05
Teslyak <124871929+Teslyak@users.noreply.github.com>
2023-06-17 12:19:29 +0300

    finish hw 6

--- js/task-07.js
+++ js/task-07.js
@@ -2,9 +2,7 @@ const refs = {
     input: document.querySelector('#font-size-control'),
     span: document.querySelector('#text')
 }
-
 refs.input.addEventListener('input', onInputChange);
-
 function onInputChange(event) {
     refs.span.style.fontSize = `${event.currentTarget.value}px `;
 }
--- js/task-10.js
+++ js/task-10.js
@@ -5,24 +5,61 @@ function getRandomHexColor() {
 }
 
 const refs = {
-  inputAmount: document.querySelector('[type=number]'),
+  divControls: document.querySelector('#controls'),
+  inputAmount: document.querySelector('div > input'),
   buttonCreate: document.querySelector('[data-create]'),
   buttonDestroy: document.querySelector('[data-destroy]'),
   divBoxes: document.querySelector('#boxes')
+}
+const min = Number(refs.inputAmount.min);
+const max = parseInt(refs.inputAmount.max);
+const step = +refs.inputAmount.step;
+
+
+refs.buttonCreate.addEventListener('click', createBoxes);
+refs.buttonDestroy.addEventListener('click', onBtnDestroy);
+refs.inputAmount.addEventListener('input', onInputValue);
+
+function onInputValue(event) {
+  if (event.currentTarget.value < min || event.currentTarget.value > max) {
+    alert(`Введіть будь ласка правильне значення в діапазоні від ${min} до ${max}`)
+  }
+};
+
+function createBoxes(amount) {
+  const size = 30;
+
+  amount = refs.inputAmount.value;
+  if (amount < min || amount > max) {
+    alert(`Ваші значення min ${min} та max ${max} не відповідають ліміту`)
+    return;
+  } else {
+    const arrBoxes = [];
+    for (let i = 0; i < amount; i += step) {
+      let reSize = size + i * 10;
+      const sizeLimit = 70;
+      const divItem = `<div style="width:${reSize >= sizeLimit ? sizeLimit : reSize}px; height:${reSize}px;
+      background-color: ${getRandomRGBColor()}; " ></div> `;
+      arrBoxes.push(divItem);
+    };
+    refs.divBoxes.insertAdjacentHTML("beforeend", arrBoxes.join(""));
+  };
+};
+
+function onBtnDestroy() {
+  refs.inputAmount.value = "";
+  refs.divBoxes.innerHTML = "";
+};
+
+const getRandomRGBColor = () => {
+  const red = Math.round(Math.random(0, 1) * 255);
+  const green = Math.round(Math.random(0, 1) * 255);
+  const blue = Math.round(Math.random(0, 1) * 255);
+  return `rgb(${red}, ${green}, ${blue})`;
 
 }
-refs.inputAmount.addEventListener('input', onInputAmount);
-refs.buttonCreate.addEventListener('click', onClickBtnCreate);
-refs.buttonDestroy.addEventListener('click', onClickBtnDestroy)
-function onInputAmount(event) {
-  const inputValue = event.currentTarget.value;
-  console.log(inputValue);
-};
 
-function onClickBtnCreate() {
-  
-};
 
-function onClickBtnDestroy() {
 
-};
+
+
